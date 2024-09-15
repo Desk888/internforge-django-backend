adaacbe2e1c66117b176f78db952acb7d8cf8937
Desk888 <work.lorenzofilippinI@gmail.com>
2024-09-15 02:11:20 +0100

    4: Adjusted main URL patterns file in project folder to point at each app's urls.py file. Created urls.py files for each service.

--- project/urls.py
+++ project/urls.py
@@ -3,5 +3,12 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('src.urls')),
+    path('api/users/', include('apps.users.urls')),
+    path('api/companies/', include('apps.companies.urls')),
+    path('api/jobs/', include('apps.jobs.urls')),
+    path('api/applications/', include('apps.applications.urls')),
+    path('api/cvs/', include('apps.cvs.urls')),
+    path('api/skills/', include('apps.skills.urls')),
+    path('api/notifications/', include('apps.notifications.urls')),
+    path('api/search/', include('apps.search.urls')),
 ]
