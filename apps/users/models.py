318175bac760e6c6abe3062b869693466b5cd22c
Desk888 <work.lorenzofilippinI@gmail.com>
2024-09-16 01:28:48 +0100

    8: Adjusted Urls.py settings for main project and each app service. Completed serializers. Created superuser and registered models in admin panel.

--- apps/users/models.py
+++ apps/users/models.py
@@ -26,3 +26,22 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.email_address})"
+    
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        related_name='custom_user_set',
+        related_query_name='custom_user',
+    )
+
+    # Override the user_permissions field
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name='custom_user_set',
+        related_query_name='custom_user',
+    )
